e4c8be222d142818f7232a270821b22e5a1b0bfc
rafadh <mrmrafadh@gmail.com>
2025-12-23 11:40:43 +0530

    55

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // --- ADD THIS FUNCTION ---
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class);
+    }
 }
